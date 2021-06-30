e41e6d4c1c07b943efb5fc7dd1f62ddc7dc8c56c
shprotaone <shprotaone@gmail.com>
2021-06-30 10:31:03 +0300

    Update Security Controller
    Security Controller Complete without Animation

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -6,24 +6,12 @@ using UnityEngine.AI;
 public class BotController  : MonoBehaviour
 {
     [SerializeField] private GameObject target;
-    [SerializeField] private GameObject localTarget;
+    [SerializeField] private bool isStopped = false;
+
     private NavMeshAgent agent;    
     private RandomPointNavMesh randomPointScript;
 
     private bool nextTarget = false;
-    [SerializeField] private bool isStopped = false;
-
-    public bool Stopped
-    {
-        get
-        {
-            return isStopped;
-        }
-        set
-        {
-            isStopped = value;          
-        }
-    }    
 
     private void Start()
     {
@@ -33,14 +21,9 @@ public class BotController  : MonoBehaviour
         StartCoroutine(Movement());
     }
 
-    private void Update()
-    {
-        VisitorStop();
-    }
-
     private IEnumerator Movement()
     {        
-        while (true)
+        while (!isStopped)
         {
             Debug.DrawLine(agent.transform.position, agent.destination,Color.red);
             if (agent.remainingDistance <= 0.01)
@@ -56,21 +39,22 @@ public class BotController  : MonoBehaviour
             yield return null;
         }              
     }
-    private void NextPoint()
+    /// <summary>
+    /// Следующая точка и возобновление движения
+    /// </summary>
+    public void NextPoint()
     {
         nextTarget = true;
         agent.SetDestination(randomPointScript.ChangePointPos(nextTarget));
         nextTarget = false;
     }
-
-    private void VisitorStop()
+    /// <summary>
+    /// Остановка
+    /// </summary>
+    public void VisitorStop()
     {
-        if (isStopped)
-        {
-            print("Oh no say: " + name);
-            agent.isStopped = true;
-            agent.ResetPath();
-        }
+        print("Oh no say: " + name);
+        isStopped = true;
+        agent.ResetPath();    
     }
-
 }
--- Assets/Scripts/SecurityController.cs
+++ Assets/Scripts/SecurityController.cs
@@ -5,25 +5,20 @@ using UnityEngine.AI;
 
 public class SecurityController : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> visitors;
-    [SerializeField] private GameObject currentVisitor;
+    [SerializeField] private List<BotController> visitors;
+    [SerializeField] private float distanceStop = 1f;
+    const float waitTime = 10f;     //уточнить, на том ли месте()
 
-    private NavMeshAgent securityAgent; 
-    
+    private NavMeshAgent securityAgent;    
     private int index;
-    private int visitorsCount;
 
-    private bool visitorStopped = false;
-    private bool cheked = false;
-
-    private void Awake()
-    {        
-        securityAgent = GetComponent<NavMeshAgent>();
-    }
     private void Start()
-    {        
-        visitors = new List<GameObject>();
-        visitors.AddRange(GameObject.FindGameObjectsWithTag("Visitor"));
+    {
+        securityAgent = GetComponent<NavMeshAgent>();
+
+        visitors = new List<BotController>();
+        visitors.AddRange(FindObjectsOfType<BotController>());
+
         NextTarget();
         StartCoroutine(CheckVisitor());
     }
@@ -35,49 +30,29 @@ public class SecurityController : MonoBehaviour
     private IEnumerator CheckVisitor()
     {
         while (true)
-        {
-            bool checkNextTarget = securityAgent.remainingDistance < 0.83f;
-            print("Условия следующей цели " + checkNextTarget);
-
-            if (securityAgent.remainingDistance < 0.83f && securityAgent.pathPending)
+        {           
+            if (securityAgent.remainingDistance < distanceStop && securityAgent.hasPath)
             {
-                yield return new WaitForSeconds(5f);
+                visitors[index].VisitorStop();
+                securityAgent.ResetPath();
+
+                yield return new WaitForSeconds(waitTime);
+
+                visitors[index].NextPoint();
                 NextTarget();                
             }
-            
             securityAgent.SetDestination(visitors[index].transform.position);
             
             yield return null;
         }
     }
-
     /// <summary>
-    /// Генерация номера следующего моба
+    /// Генерация номера следующего посетителя
     /// </summary>
     /// <returns></returns>
     private int NextTarget()
     {
-        visitorsCount = visitors.Count;
-        index = Random.Range(0, visitorsCount);
-        cheked = true;
-        return index;       
-    }
-    /// <summary>
-    /// Попытка остановить при касании
-    /// </summary>
-    /// <param name="collision"></param>
-    private void OnCollisionEnter(Collision collision)
-    {
-        BotController botController;        
-        currentVisitor = collision.gameObject;
-       
-        if (currentVisitor != null && currentVisitor.name == visitors[index].name)  //2nd
-        {
-            print(collision.collider.name);
-            botController = currentVisitor.GetComponent<BotController>();
-            botController.Stopped = true;
-            cheked = true;
-            visitors.RemoveAt(index);
-        }        
+        index = Random.Range(0, visitors.Count);
+        return index;
     }
 }
