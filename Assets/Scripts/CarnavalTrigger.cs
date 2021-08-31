0b52689b127d1b6132899a5c176c2c8b8b3eb8cf
shprotaone <shprotaone@gmail.com>
2021-08-31 21:22:44 +0300

    Complete

---
+++ Assets/Scripts/CarnavalTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarnavalTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioSource mainSound;
+    [SerializeField] private GameObject carnaval;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            mainSound.Play();
+            carnaval.SetActive(true);
+        }
+    }
+}
